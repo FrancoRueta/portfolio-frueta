f5633d048552517e9fe3e4a5addb0f353559e541
FrancoRueta <askjfuhje@hotmail.com>
2024-09-25 21:11:36 -0300

    removed extra files, possible bugfix on cv.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import linkedinIconL from '../../assets/linkedin-light.svg'
 import twitterIconD from '../../assets/twitter-dark.svg'
 import githubIconD from '../../assets/github-dark.svg'
 import linkedinIconD from '../../assets/linkedin-dark.svg'
-import CV from '../../assets/CV.pdf'
+import cv from '../../assets/cv.pdf'
 import { useTheme } from '../../common/ThemeContext'
 
 function Hero() {
@@ -43,7 +43,7 @@ function Hero() {
           </a>
         </span>
         <p className={styles.description}>With a passion for developing modern React web apps for commercial businesses.</p>
-        <a href={CV} download>
+        <a href={cv} download>
           <button className="hover">
             Resume
           </button>
